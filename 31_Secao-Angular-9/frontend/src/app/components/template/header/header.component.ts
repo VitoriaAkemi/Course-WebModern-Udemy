6ae8d2bbf1a58d4e733821ed0c99917767462abd
Vitoria Akemi <vitoriaakemitanimura@gmail.com>
2023-05-03 19:32:10 -0300

    criando header

--- 31_Secao-Angular-9/frontend/src/app/components/template/header/header.component.ts
+++ 31_Secao-Angular-9/frontend/src/app/components/template/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'app-header', // nome do component
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
